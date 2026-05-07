70589e2f92afecb176f7ff8d5c2fb06315282688
Bakr27 <nafady2703@gmail.com>
2026-05-07 23:14:02 +0300

    ✨ Update portfolio content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ export default function Contact() {
             {/* CV Download */}
             <motion.a
               href={personalInfo.cvUrl}
-              download
+              download={personalInfo.cvFilename}
               initial={{ opacity: 0, y: 20 }}
               animate={inView ? { opacity: 1, y: 0 } : {}}
               transition={{ duration: 0.5, delay: 0.7 }}
