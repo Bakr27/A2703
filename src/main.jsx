6e02c8e7cf4650ca061b957a83061b4fe3fb8c27
Bakr27 <nafady2703@gmail.com>
2026-05-07 22:18:21 +0300

    🔧 استيراد favicon من src

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import favicon from './favicon.svg'
+
+// Set favicon dynamically
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/svg+xml'
+link.href = favicon
+document.head.appendChild(link)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
